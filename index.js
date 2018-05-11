284bf18b5bc5a90f9941b56e844987c0689ec2f0
benyoo5222 <benyoo5222@gmail.com>
2018-05-11 03:12:02 +0000

    Learned recursive algorithem

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
 function sum(fromN, toN) {
+  var total = 0;
   // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN;
+  }
+  // total += toN;
+  total += toN + sum(fromN, toN-1);
+  return total;
+
 }
+
 
 module.exports = sum;
